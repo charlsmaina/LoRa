7ef7128cbb40a1a626ccd3b20ca95377a17033f0
charlsmaina <mainacharles1496@gmail.com>
2026-04-15 07:51:55 +0300

    SPI test comments

--- src/tests/SPI.cpp
+++ src/tests/SPI.cpp
@@ -1,18 +1,31 @@
-#include "../include/pin_config.h"
+/*
+---------------------------------------------------------------------------------------------------------------
+This sketch is a test to check if SPI is working between the sx1276 chip and the esp32
+-------How it works----------------
+- SPI.h provides with the SPI transport layer implementation. More like HAL implementation. How the Hardware is manipulated to send data
+- SPI is a simultaneos two way protocol. As master sends to slave, master receives from slave
+- For sx1276, SPI works by sending two bytes successively. First byte is the register address to write to or read from with MSB indicating if its a write or read operation
+- Second byte contains the data. For read , you send a dummy data, and the sx1276 MISO sends the data stored in the specified address.
+- The whole read or write operation must be executed while the chip is selected: ie NSS should be low: NB: NSS is active LOW
+
+--------------------------------------------------------------------------------------------------------------
+*/
+
+#include "../../include/pin_config.h" // contains SPI pin mappings to ESP32
 #include <Arduino.h>
 #include <SPI.h>
-// put function declarations here:
+
 uint8_t readReg(uint8_t addr)
 {
     digitalWrite(PIN_NSS, LOW);
-    SPI.transfer(addr & 0x7F);
-    uint8_t val = SPI.transfer(0x00);
+    SPI.transfer(addr & 0x7F);        // 0x7F = 0111111 : hence MSB is 0 for read
+    uint8_t val = SPI.transfer(0x00); // save returned value via MISO
     digitalWrite(PIN_NSS, HIGH);
     return val;
 }
 void setup()
 {
-    // put your setup code here, to run once:
+
     Serial.begin(115200);
     pinMode(PIN_NSS, OUTPUT);
     digitalWrite(PIN_NSS, HIGH);
