786c0c190055ad211c6525f9857aff31cc3592a6
charlsmaina <mainacharles1496@gmail.com>
2026-04-15 07:08:01 +0300

    init

---
+++ src/lora/lora_registers.h
@@ -0,0 +1,9 @@
+/*
+----------------------------------------------------------------------
+This file contain a collection of sx1276 registers addresses
+----------------------------------------------------------------------
+*/
+#ifndef LORA_REGISTERS_H
+#define LORA_REGISTERS_H
+
+#endif
