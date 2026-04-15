566eb46dc4c81632b6d358afec7fbfd7432a465c
charlsmaina <mainacharles1496@gmail.com>
2026-04-15 15:27:26 +0300

    rearragement'

---
+++ include/lora/pin_config.h
@@ -0,0 +1,16 @@
+
+/*
+-------------------------------------------------------------------------
+This file contains the physical wiring mapping between the rfm95 and the esp32
+-------------------------------------------------------------------------
+*/
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+#define PIN_NSS 5
+#define PIN_RESET 14
+#define PIN_DIO0 26
+#define PIN_MISO 19
+#define PIN_MOSI 23
+#define PIN_SCK 18
+#endif
---
+++ include/lora/tests.h
@@ -0,0 +1,12 @@
+#ifndef TESTS_H
+#define TESTS_H
+#include <stdint.h>
+#include "../lora/lora_config.h"
+
+void writeRegister(uint8_t addr, uint8_t value);
+uint8_t readRegister(uint8_t addr);
+void lora_hardware_reset(void);
+bool lora_init(void);
+void printAllRegisters();
+void set_Mode(lora_mode_t);
+#endif
