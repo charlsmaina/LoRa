e097c0d2000ee43d36086ab41015e4c6353dec51
charlsmaina <mainacharles1496@gmail.com>
2026-04-15 11:06:08 +0300

    file restructuring

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,20 @@
-#include "../include/pin_config.h"
+
+#include "../include/pin_config.h" // contains SPI pin mappings to ESP32
 #include <Arduino.h>
 #include <SPI.h>
-// put function declarations here:
-uint8_t readReg(uint8_t addr)
-{
-  digitalWrite(PIN_NSS, LOW);
-  SPI.transfer(addr & 0x7F);
-  uint8_t val = SPI.transfer(0x00);
-  digitalWrite(PIN_NSS, HIGH);
-  return val;
-}
+#include "tests/tests.h"
+
 void setup()
 {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
-  pinMode(PIN_NSS, OUTPUT);
-  digitalWrite(PIN_NSS, HIGH);
 
-  pinMode(PIN_RESET, OUTPUT);
-  digitalWrite(PIN_RESET, HIGH);
+  lora_hardware_reset();
 
-  SPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI, PIN_NSS);
-  SPI.setFrequency(1000000);
-
-  // Hardware reset
-  digitalWrite(PIN_RESET, LOW);
-  delay(10);
-  digitalWrite(PIN_RESET, HIGH);
-  delay(10);
-
-  uint8_t version = readReg(0x42);
-  Serial.printf("Reg version = 0x%02X\n", version);
-  if (version == 0x12)
-    Serial.println("SPI OK!");
+  if (lora_init)
+    Serial.printf("Initialization successfull!\nSPI OK!\n");
   else
-    Serial.println("SPI FAIL!!");
+    Serial.printf("Initialization failed!\nSPI not working!!\n");
 }
 
 void loop() {}
