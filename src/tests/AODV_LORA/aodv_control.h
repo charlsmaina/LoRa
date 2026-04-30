3a1a918d3291f5a297e718b2ca1df74c1dbaeaaf
charlsmaina <mainacharles1496@gmail.com>
2026-04-30 07:00:21 +0300

    aodv control messages format

---
+++ src/tests/AODV_LORA/aodv_control.h
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+#ifndef AODV_CONTROL_H
+#define AODV_CONTROL_H
+
+typedef struct
+{
+    uint8_t type;
+    uint8_t flags;
+    uint8_t hop_count;
+    uint8_t rreq_id;
+    char dest_ip;
+    uint8_t dest_seq_number;
+    uint8_t ori_ip;
+    uint8_t ori_seq_number;
+} RREQ_MESSAGE_t;
+
+typedef struct
+{
+    uint8_t type;
+    uint8_t flags;
+    uint8_t hop_count;
+    uint8_t dest_ip;
+    uint8_t dest_seq_number;
+    uint8_t ori_ip;
+    uint8_t lifetime;
+};
+
+#endif
