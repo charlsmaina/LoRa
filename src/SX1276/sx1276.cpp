d0a4a6483132f76557b3f5869dfc122d1215e4bb
charlsmaina <mainacharles1496@gmail.com>
2026-05-03 09:40:17 +0300

    sx1276 driver refactoring

---
+++ src/SX1276/sx1276.cpp
@@ -0,0 +1,4 @@
+
+/*This file contains a simple sx1276 driver to implement a usable physical layer{ The layer is concered with that sending /receiving functionality}*/
+#include "../../include/lora/lora_registers.h"
+#include "../../include/lora/pin_config.h"
