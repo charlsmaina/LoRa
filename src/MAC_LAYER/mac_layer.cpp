7279b98bcda89a5077603692f2bca34dacd9c505
charlsmaina <mainacharles1496@gmail.com>
2026-05-07 12:55:10 +0300

    request and clear to send functions

--- src/MAC_LAYER/mac_layer.cpp
+++ src/MAC_LAYER/mac_layer.cpp
@@ -10,16 +10,22 @@ static uint8_t tx_buffer[MAX_FRAME_SIZE];
 static payload_cb_t payload_mac_aodv_cb = NULL;
 static rreq_cb_t rreq_mac_aodv_cb = NULL;
 static rrep_cb_t rrep_mac_aodv_cb = NULL;
+static rts_cb_t rts_mac_aodv_cb = NULL;
+static cts_cb_t cts_mac_aodv_cb = NULL;
+static ack_cb_t ack_mac_aodv_cb = NULL;
 
 static void mac_on_tx_done(void);
 static void mac_on_rx_done(void);
 
-void mac_init(payload_cb_t payload_handler, rreq_cb_t rreq_handler, rrep_cb_t rrep_handler)
+void mac_init(payload_cb_t payload_handler, rreq_cb_t rreq_handler, rrep_cb_t rrep_handler, rts_cb_t rts_handler, cts_cb_t cts_handler, ack_cb_t ack_handler)
 {
     radio_ini();
     payload_mac_aodv_cb = payload_handler;
     rreq_mac_aodv_cb = rreq_handler;
     rrep_mac_aodv_cb = rrep_handler;
+    rts_mac_aodv_cb = rts_handler;
+    cts_mac_aodv_cb = cts_handler;
+    ack_mac_aodv_cb = ack_handler;
 
     radio_init(mac_on_tx_done, mac_on_rx_done);
 }
@@ -31,11 +37,23 @@ void mac_forward(uint8_t *buf, uint8_t len)
         transmit(buf, len);
         break;
     case RREP_MSG:
-        transmit(buf, sizeof(RREP_MESSAGE_t));
+        transmit(buf, len);
         break;
     case RREQ_MSG:
-        transmit(buf, sizeof(RREQ_MESSAGE_t));
+        transmit(buf, len);
         Serial.printf("RREQ forwarded to radio layer to be sent\n");
+        break;
+    case RTS_MSG:
+        transmit(buf, len);
+        Serial.printf("A RTS message forwaded to radio layer by MAC\n");
+        break;
+    case CTS_MSG:
+        transmit(buf, len);
+        Serial.printf("A CTS message forwaded for transmit\n");
+        break;
+    case ACK_MSG:
+        transmit(buf, len);
+        Serial.printf("An acknowledgement message forwaded to radio layer by MAC:\n");
         break;
 
     default:
@@ -81,10 +99,28 @@ static void mac_on_rx_done(void)
         break;
     case RTS_MSG:
         /*Call a aodv function to handle rts*/
+        if (rts_mac_aodv_cb)
+        {
+            Serial.printf("RTS message detected:\n");
+            rts_mac_aodv_cb((RTS_MESSAGE_t *)payload_pointer);
+        }
+
         break;
-    case RTR_MSG:
+    case CTS_MSG:
         /*Call a aodv function to handle rtr*/
+        if (cts_mac_aodv_cb)
+        {
+            Serial.printf("CTS message detected:\n");
+            cts_mac_aodv_cb((CTS_MESSAGE_t *)payload_pointer);
+        }
+
         break;
+    case ACK_MSG:
+        if (ack_mac_aodv_cb)
+        {
+            Serial.printf("ACK message detected:\n");
+            ack_mac_aodv_cb((ACK_MESSAGE_t *)payload_pointer);
+        }
 
     default:
         Serial.printf("Undefined payload type:\n");
@@ -105,15 +141,38 @@ void mac_send_payload(uint8_t *buf, uint8_t len)
 void mac_send_rreq(RREQ_MESSAGE_t *rreq)
 {
     Serial.printf("MAC layer sending a route request:\n");
+
     rreq->type = RREQ_MSG;
-    memcpy(tx_buffer, rreq, sizeof(RREQ_MESSAGE_t));
-    mac_forward(tx_buffer, sizeof(RREQ_MESSAGE_t));
+
+    tx_buffer[0] = RREQ_MSG;
+    memcpy(tx_buffer + 1, rreq, sizeof(RREQ_MESSAGE_t));
+    mac_forward(tx_buffer, sizeof(RREQ_MESSAGE_t) + 1);
 }
 void mac_send_rrep(RREP_MESSAGE_t *rrep)
 {
     rrep->type = RREP_MSG;
-    memcpy(tx_buffer, rrep, sizeof(RREP_MESSAGE_t));
-    mac_forward(tx_buffer, sizeof(RREP_MESSAGE_t));
+    tx_buffer[0] = RREP_MSG;
+    memcpy(tx_buffer + 1, rrep, sizeof(RREP_MESSAGE_t));
+    mac_forward(tx_buffer, sizeof(RREP_MESSAGE_t) + 1);
+}
+void mac_send_rts(RTS_MESSAGE_t *rts)
+{
+    tx_buffer[0] = RTS_MSG;
+    memcpy(tx_buffer + 1, rts, sizeof(RTS_MESSAGE_t));
+    mac_forward(tx_buffer, sizeof(RTS_MESSAGE_t) + 1);
+}
+void mac_send_cts(CTS_MESSAGE_t *cts)
+{
+    tx_buffer[0] = CTS_MSG;
+    memcpy(tx_buffer + 1, cts, sizeof(CTS_MESSAGE_t));
+    mac_forward(tx_buffer, sizeof(RTS_MESSAGE_t) + 1);
+}
+
+void mac_send_ack(ACK_MESSAGE_t *ack)
+{
+    tx_buffer[0] = ACK_MSG;
+    memcpy(tx_buffer + 1, ack, sizeof(ACK_MESSAGE_t));
+    mac_forward(tx_buffer, sizeof(ACK_MESSAGE_t) + 1);
 }
 
 void mac_tick(void)
